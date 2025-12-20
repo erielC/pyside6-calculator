05d84e052ceb79141e4fb380aedb23f9e463fa73
Eriel Cabrera <eriel.dev@pm.me>
2025-12-19 23:45:53 -0500

    reorganized imports and changed entry point location

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@
 Calculator Application Main Entry Point
 """
 
-from frontend import main  # Ensure 'frontend' is the correct module where 'main' is defined
+from calculator.frontend import main  # Ensure 'frontend' is the correct module where 'main' is defined
 
 if __name__ == '__main__':
     main()  # Call the main function to start the application
